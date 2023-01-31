eb6c843747013c3e8d2df06cf58d7126f3942e1c
mejta <daniel@mejta.net>
2023-01-31 18:20:13 +0100

    WIP

--- skeleton/web/app/mu-plugins/x-switch-theme.php
+++ skeleton/web/app/mu-plugins/x-switch-theme.php
@@ -1,5 +1,5 @@
 <?php
-add_action( 'muplugins_loaded', function () {
+add_action( 'setup_theme', function () {
 	if ( ! defined( 'WP_INSTALLING' ) || ! WP_INSTALLING ) {
 		switch_theme( 'wpify-skeleton' );
 		unlink( __FILE__ );
